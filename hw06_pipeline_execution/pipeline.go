73e29c5e40ba79cbdb6d6fb64d85c88819f62e94
samatmurat <muratsamat090598@gmail.com>
2025-01-23 12:20:49 +0500

    Revert ": added logic for pipeline"
    This reverts commit 1dd1119a5889be5b72d42d5312f610e6457578a2.

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,32 +9,6 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	doneWrapper := func(in In) Out {
-		out := make(Bi)
-		go func() {
-			defer close(out)
-			for {
-				select {
-				case <-done: 
-					return
-				case v, ok := <-in: 
-					if !ok { 
-						return
-					}
-					select {
-					case <-done: 
-						return
-					case out <- v:
-					}
-				}
-			}
-		}()
-		return out
-	}
-
-	for _, stage := range stages {
-		in = doneWrapper(stage(in))
-	}
-
-	return in
+	// Place your code here.
+	return nil
 }
