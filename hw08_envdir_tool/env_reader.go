8d89290e3947d0f30829d62a25bf4ce7b9b6fb17
samatmurat <muratsamat090598@gmail.com>
2025-02-14 01:39:00 +0500

    : changed byte replace method

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -67,7 +67,7 @@ func readFile(dir string, fileName string, environment Environment) (err error) 
 
 	if scanner.Scan() {
 		environment[fileName] = EnvValue{
-			Value: strings.TrimRight(string(bytes.Replace(scanner.Bytes(), []byte{0x00}, []byte("\n"), -1)), "\t &nbsp"),
+			Value: strings.TrimRight(string(bytes.ReplaceAll(scanner.Bytes(), []byte{0x00}, []byte("\n"))), "\t &nbsp"),
 		}
 	} else if err = scanner.Err(); err != nil {
 		return
