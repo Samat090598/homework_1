f7521dfffcb3a59ead782f0a69cec7e81513094e
samatmurat <muratsamat090598@gmail.com>
2025-02-14 01:22:01 +0500

    : added logic in executor.go

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"errors"
-	"fmt"
 	"log/slog"
 	"os"
 )
 
 var (
-	ErrUnsupportedFile = errors.New("the required argument was not found")
+	ErrNotEnoughArguments = errors.New("not enough arguments")
 )
 
 func main() {
 	args := os.Args
 
-	if len(args) < 2 {
-		slog.Error(ErrUnsupportedFile.Error())
+	if len(args) < 4 {
+		slog.Error(ErrNotEnoughArguments.Error())
 		os.Exit(1)
 	}
 
@@ -25,29 +24,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	for key, val := range environment {
-		if !val.NeedRemove {
-			_, ok := os.LookupEnv(key)
-			if ok {
-				err := os.Unsetenv(key)
-				if err != nil {
-					slog.Error(fmt.Errorf("unset env err: %w", err).Error())
-				}
-			}
-
-			err := os.Setenv(key, val.Value)
-			if err != nil {
-				slog.Error(fmt.Errorf("set env err: %w", err).Error())
-			}
-		} else {
-			err := os.Unsetenv(key)
-			if err != nil {
-				slog.Error(fmt.Errorf("unset env err: %w", err).Error())
-			}
-		}
-	}
-
-	for _, val := range os.Environ() {
-		fmt.Println(val)
-	}
+	_ = RunCmd(args[2:], environment)
 }
