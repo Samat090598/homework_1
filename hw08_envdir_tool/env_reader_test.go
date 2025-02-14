75cec92d92a79b745128c13b221f8154ef052867
samatmurat <muratsamat090598@gmail.com>
2025-02-14 23:58:16 +0500

    : added simple test to env_reader

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,40 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	dir, _ := os.Getwd()
+
+	dir = path.Join(dir, "testdata/env")
+
+	t.Run("simple case", func(t *testing.T) {
+		environment := Environment{
+			"EMPTY": EnvValue{
+				Value: "",
+			},
+			"FOO": EnvValue{
+				Value: `   foo
+with new line`,
+			},
+			"HELLO": EnvValue{
+				Value: `"hello"`,
+			},
+			"UNSET": EnvValue{
+				NeedRemove: true,
+			},
+			"BAR": EnvValue{
+				Value: `bar`,
+			},
+		}
+
+		result, _ := ReadDir(dir)
+
+		require.Equal(t, environment, result)
+	})
 }
